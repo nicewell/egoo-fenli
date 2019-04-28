f395074db1a0cd8cd4daf79b67e7779b98c84576
duang <niceduang@qq.com>
2019-04-28 18:30:41 +0800

    :sparkles:egoo admin

--- egoo-fenli-admin/src/main.js
+++ egoo-fenli-admin/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'iview/dist/styles/iview.css'
+
 Vue.config.productionTip = false
 
 new Vue({
--- egoo-fenli-admin/src/views/Home.vue
+++ egoo-fenli-admin/src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="nav">
+      <div class="nav-start">
+        <a href="javascript:void(0);" class="logo" title="Egoo cli">
+          <img src="../assets/img/egoo_cli.svg">
+          <span><em>Egoo CLI</em><i>分离地址管理</i></span>
+        </a>
+      </div>
+      <div class="nav-end">
+
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Vue from 'vue'
+import { Button } from 'iview'
+Vue.component('Button', Button)
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: 'Home',
+  compontes: {
+    // Button
+  },
+  data () {
+    return {
+      //
+    }
+  },
+  methods: {
+    //
   }
 }
 </script>
+<style lang="scss" scoped>
+.nav{
+  position: relative;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+  height: 60px;
+  &-start{
+    position: relative;
+  }
+  .logo{
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: flex-start;
+    align-items: center;
+  }
+  &-end{}
+}
+</style>
