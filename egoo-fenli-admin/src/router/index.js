b7324ad375343dbad0c93595c69bfbcf7cd3ab09
duang <niceduang@qq.com>
2019-05-14 11:14:15 +0800

    :bookmark:路由守卫

--- egoo-fenli-admin/src/router/index.js
+++ egoo-fenli-admin/src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import store from '@/store'
 
 Vue.use(Router)
-
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -30,3 +30,22 @@ export default new Router({
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  // 获取token
+  const token = store.state.user.token
+  const pass = ['/login', '/register']
+  if (pass.includes(to.path)) {
+    next()
+  } else {
+    token
+      ? next()
+      : next({
+        path: '/login',
+        query: {
+          redirect: to.fullPath // 将刚刚要去的路由path（却无权限）作为参数，方便登录成功后直接跳转到该路由
+        }
+      })
+  }
+})
+export default router
